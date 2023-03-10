23fcbbd6b7f743d56d0a1c84a0def66559197b38
LETICIA ARAUJO PEREIRA <leticia.1970@aluno.pr.senac.br>
2023-03-10 08:08:40 -0300

    par

---
+++ par.php
@@ -0,0 +1,10 @@
+<?php   for ($par = 0; $par<=500; $par++){
+if (($par % 2 == 1) and ($par <500)){
+            continue;
+}elseif ($par <=500) {
+        echo $par . "-";
+}else{
+    break;
+}
+}
+?>
