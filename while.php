04247371578671706a973deb2f6808880cea2f77
LETICIA ARAUJO PEREIRA <leticia.1970@aluno.pr.senac.br>
2023-03-10 08:02:16 -0300

    mensagem

---
+++ while.php
@@ -0,0 +1,73 @@
+<?php
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    $d = 4;
+    $e = 5;
+    $f = 6;
+    $g = 7;
+    $h = 8;
+    $i = 9;
+    $j = 10;
+
+    do{
+        echo "número" . $a . "</br>";
+        $a = $a +1;
+    }
+    while ($a <= 10);
+    echo ("</br>");
+    do{
+        echo "número" . $b . "</br>";
+        $b = $b +2;
+    }
+    while ($b <= 20);
+    echo ("</br>");
+    do{
+        echo "número" . $c . "</br>";
+        $c = $c +3;
+    }
+    while ($c <= 30);
+    echo ("</br>");
+    do{
+        echo "número" . $d . "</br>";
+        $d = $d +4;
+    }
+    while ($d <= 40);
+    echo ("</br>");
+    do{
+        echo "número" . $e . "</br>";
+        $e = $e +5;
+    }
+    while ($e <= 50);
+    echo ("</br>");
+    do{
+        echo "número" . $f . "</br>";
+        $f = $f +6;
+    }
+    while ($f <= 60);
+    echo ("</br>");
+    do{
+        echo "número" . $g . "</br>";
+        $g = $g +7;
+    }
+    while ($g <= 70);
+    echo ("</br>");
+     do{
+        echo "número" . $h . "</br>";
+        $h = $h +8;
+    }
+    while ($h <= 80);
+    echo ("</br>");
+    do{
+        echo "número" . $i . "</br>";
+        $i = $i +9;
+    }
+    while ($i <= 90);
+    echo ("</br>");
+     do{
+        echo "número" . $j . "</br>";
+        $j = $j +10;
+    }
+    while ($j <= 100);
+    echo ("</br>");
+    ?>
